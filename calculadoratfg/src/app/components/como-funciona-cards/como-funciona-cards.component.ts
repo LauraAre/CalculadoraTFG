7eb0f02bd60d593940c4985ceb373ed2bf387871
LauraAre <lauritadefuensa@gmail.com>
2025-05-16 13:35:43 +0200

    ComoFuncionaTerminada-FaltaResponsivo

--- calculadoratfg/src/app/components/como-funciona-cards/como-funciona-cards.component.ts
+++ calculadoratfg/src/app/components/como-funciona-cards/como-funciona-cards.component.ts
@@ -1,11 +1,17 @@
-import { Component } from '@angular/core';
-
+import { Component, DoCheck } from '@angular/core';
 @Component({
   selector: 'app-como-funciona-cards',
+  standalone: true,
   imports: [],
   templateUrl: './como-funciona-cards.component.html',
   styleUrl: './como-funciona-cards.component.scss'
 })
-export class ComoFuncionaCardsComponent {
+export class ComoFuncionaCardsComponent implements DoCheck {
+  // Esto es para saber si el modo oscuro está activado o no
+  // y sirve para cambiar las imagenes de inicio.component.html
+  isDarkMode = false;
+  ngDoCheck(): void {
+    this.isDarkMode = document.documentElement.classList.contains('dark-mode');
+  }
 
 }
--- calculadoratfg/src/app/paginas/como-funciona/como-funciona.component.ts
+++ calculadoratfg/src/app/paginas/como-funciona/como-funciona.component.ts
@@ -1,7 +1,9 @@
 import { Component } from '@angular/core';
-
+import { ComoFuncionaCardsComponent } from '../../components/como-funciona-cards/como-funciona-cards.component';
 @Component({
   selector: 'app-como-funciona',
+  standalone: true,
+  imports: [ComoFuncionaCardsComponent],
   templateUrl: './como-funciona.component.html',
   styleUrls: ['./como-funciona.component.scss']
 })
