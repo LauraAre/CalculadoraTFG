5b32aad618fdb920342b8337e800dd555161c7e6
LauraAre <lauritadefuensa@gmail.com>
2025-05-18 20:00:42 +0200

    AccesibilidadTerminada-FaltaResponsivo

--- calculadoratfg/src/app/paginas/accesibilidad/accesibilidad.component.ts
+++ calculadoratfg/src/app/paginas/accesibilidad/accesibilidad.component.ts
@@ -1,11 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, DoCheck } from '@angular/core';
 
 @Component({
   selector: 'app-accesibilidad',
   standalone: true,
+  imports: [],
   templateUrl: './accesibilidad.component.html',
   styleUrls: ['./accesibilidad.component.scss']
 })
-export class AccesibilidadComponent {
-
+export class AccesibilidadComponent implements DoCheck {
+  // Esto es para saber si el modo oscuro está activado o no
+  // y sirve para cambiar las imagenes de inicio.component.html
+  isDarkMode = false;
+  ngDoCheck(): void {
+    this.isDarkMode = document.documentElement.classList.contains('dark-mode');
+  }
 }
