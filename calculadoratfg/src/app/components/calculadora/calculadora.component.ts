c3d239eaf01ced222bc3f88555af306c78f36b6d
LauraAre <lauritadefuensa@gmail.com>
2025-05-25 20:53:12 +0200

    UltimosArreglos

--- calculadoratfg/src/app/components/calculadora/calculadora.component.ts
+++ calculadoratfg/src/app/components/calculadora/calculadora.component.ts
@@ -10,8 +10,8 @@ import { CommonModule } from '@angular/common'; // Importo CommonModule para usa
 })
 export class CalculadoraComponent implements OnInit {
 
-  operacion: string = ''; 
-  resultado: string = '0'; 
+  operacion: string = '';
+  resultado: string = '0';
   mensajeError: string = ''; // Usado para mostrar errores en <app-mensaje-error>
 
   reconocimientoVoz: any; // Variable para el reconocimiento de voz
@@ -22,33 +22,33 @@ export class CalculadoraComponent implements OnInit {
     if (SpeechRecognition) {
       this.reconocimientoVoz = new SpeechRecognition();
       this.reconocimientoVoz.lang = 'es-ES'; // Idioma en español
-      this.reconocimientoVoz.continuous = false; 
+      this.reconocimientoVoz.continuous = false;
       this.reconocimientoVoz.interimResults = false; // No quiero resultados intermedios
 
       this.reconocimientoVoz.onresult = (event: any) => {
         const vozTexto = event.results[0][0].transcript; // Obtengo el texto sacado de mi API
-        console.log('Texto reconocido:', vozTexto);
-        
-      if (/^\d+\s+\d+$/.test(vozTexto)) {
-    this.operacion = vozTexto.replace(/\s+/, '/');
-  } else {
-    this.operacion = this.convertirTexto(vozTexto); // Convierto el texto de voz a una operación matemática
-  }
+        console.log('Texto escuchado por la API:', vozTexto);
 
-  this.mensajeError = '';
-  this.calcular();
-};
+        if (/^\d+\s+\d+$/.test(vozTexto)) {
+          this.operacion = vozTexto.replace(/\s+/, '/');
+        } else {
+          this.operacion = this.convertirTexto(vozTexto); // Convierto el texto de voz a una operación matemática
+        }
+
+        this.mensajeError = '';
+        this.calcular();
+      };
 
       //Manejo de errores, en el caso de que no se pueda reconocer la voz:
       this.reconocimientoVoz.onerror = (event: any) => {
-        this.mensajeError = 'Error al reconocer la voz. Vuelve a intentarlo más tarde.'; 
+        this.mensajeError = 'Error al reconocer la voz. Vuelve a intentarlo más tarde.';
       };
     } else {
-      this.mensajeError = 'Lo siento, este navegador no soporta el reconocimiento de voz.';  
+      this.mensajeError = 'Lo siento, este navegador no soporta el reconocimiento de voz.';
     }
   }
 
-// Agrego los números o los símbolos a la operación (Botones 0-9, +, -, *, /)
+  // Agrego los números o los símbolos a la operación (Botones 0-9, +, -, *, /)
   agregarValor(valor: string) {
     this.operacion += valor;
     this.mensajeError = '';
@@ -69,14 +69,12 @@ export class CalculadoraComponent implements OnInit {
         this.mensajeError = 'Operación no válida'; //  Si la operación no es válida, se muestra un mensaje de error
       }
       this.mensajeError = '';
+      this.decirResultado(this.resultado);
     } catch {
-      this.resultado = '0'; 
-      this.mensajeError = 'No se puede hacer la operación'; 
+      this.resultado = '0';
+      this.mensajeError = 'No se puede hacer la operación';
     }
   }
-
-
-
 
   microActivo: boolean = false;
   escucharVoz() {
@@ -92,13 +90,11 @@ export class CalculadoraComponent implements OnInit {
     } else {
       this.reconocimientoVoz.stop();
       this.microActivo = false;
-      // Esto lo hago para forzar el cálculo:
-      this.calcular();
     }
   }
 
 
-// Convierto los operadores de texto escuchados, a símbolos matemáticos
+  // Convierto los operadores de texto escuchados, a símbolos matemáticos
   convertirTexto(texto: string): string {
     return texto
       .toLowerCase()
@@ -108,5 +104,17 @@ export class CalculadoraComponent implements OnInit {
       .replace(/entre/g, '/')
       .replace(/ /g, '');
   }
-}
 
+
+  decirResultado(resultado: string) {
+    if ('speechSynthesis' in window) {
+      const mensaje = new SpeechSynthesisUtterance(`El resultado es ${resultado}`);
+      mensaje.lang = 'es-ES'; // Idioma de la voz
+      mensaje.rate = 1; // Velocidad de la voz
+      mensaje.pitch = 1; // Tono de la voz
+      window.speechSynthesis.speak(mensaje); // Dice el mensaje (la constante de arriba)
+    } else {
+      this.mensajeError = 'Lo sentimos, tu navegador no puede hablar.';
+    }
+  }
+}
